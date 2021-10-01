49dcab0ed2162ba2d12530a2554d91f5f0d36e41
lorenzorru0 <lorenzoorru999@gmail.com>
2021-10-01 16:22:32 +0200

    Add main

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,15 +1,42 @@
 <template>
-    <div>
-        
+    <div class="album">
+        <img :src="album.poster" :alt="album.author + album.title">
+        <h2>{{album.title}}</h2>
+        <p>{{album.author}}</p>
+        <p>{{album.year}}</p>
     </div>
 </template>
 
 <script>
 export default {
-
+    name: 'Album',
+    props: ['album']
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '../assets/style/variable.scss';
 
+.album {
+    width: $widthAlbum;
+    margin: 1.25rem;
+    padding: 1.25rem;
+    background-color: $colorBgHeader;
+
+    img {
+        width: 100%;
+    }
+
+    h2 {
+        padding: 1.25rem 0;
+        text-align: center;
+        color: #fff;
+        text-transform: uppercase;
+    }
+
+    p {
+        text-align: center;
+        color: #808080;
+    }
+}
 </style>
--- src/components/AlbumContainer.vue
+++ src/components/AlbumContainer.vue
@@ -1,15 +1,45 @@
 <template>
     <section>
-
+        <div class="container">
+            <Album v-for="(album, index) in albums" :key="index" :album="album"/>
+        </div>
     </section>
 </template>
 
 <script>
+import axios from 'axios';
+import Album from './Album.vue'
+
 export default {
-    name: 'AlbumContainer'
+    name: 'AlbumContainer',
+    components: {
+        Album
+    },
+    data() {
+        return {
+            albums: []
+        }
+    },
+    created() {
+        axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+            .then( (res) => {
+                this.albums = res.data.response;
+                console.log(this.albums)
+            });
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '../assets/style/variable.scss';
 
+section {
+    background-color: $colorBgBody;
+    padding: 3.125rem 0;
+
+    .container {
+        display: flex;
+        flex-wrap: wrap;
+    }
+}
 </style>
